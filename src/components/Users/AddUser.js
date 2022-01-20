f9ef2390a0d7ad87b0268d245ddb66fd397551a8
Laxita <86618594+laxitabaheti@users.noreply.github.com>
2022-01-20 16:20:01 +0530

    Second Commit

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,3 +1,5 @@
+import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
 const AddUser = (props) => {
   const SubmitHandler = (event) => {
     event.preventDefault();
@@ -5,13 +7,15 @@ const AddUser = (props) => {
 
   return (
     <div>
-      <form onSubmit={SubmitHandler}>
-        <label>Username</label>
-        <input id="username" type="text" />
-        <label>Age (Years)</label>
-        <input id="age" type="number" />
-        <button type="submit">Add User</button>
-      </form>
+      <Card className={styles.card}>
+        <form onSubmit={SubmitHandler}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" />
+          <label htmlFor="age">Age (Years)</label>
+          <input id="age" type="number" />
+          <button type="submit">Add User</button>
+        </form>
+      </Card>
     </div>
   );
 };
